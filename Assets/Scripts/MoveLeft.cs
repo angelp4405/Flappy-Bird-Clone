06bd8959e7d14441fda91193ea7575dd14f33e54
angel <avasquez27@aoiths.org>
2025-06-09 13:11:58 -0400

    Made the Game Stop Pipe and Background Movement on Game Over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
-
 {
+    //Creating a varible to store the PlayerController script
+    PlayerController playerControllerScript;
     public float speed;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        // Find the player controller script when the game starts
+        playerControllerScript = GameObject.Find("Red_Bird_0").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(playerControllerScript.isAlive == true)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+        else
+        {
+            speed = 0;
+        }
+
         //Move pipes left
         transform.Translate(Vector2.left * Time.deltaTime * speed);
     }
